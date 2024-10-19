f4559a37548fbff85ab6cd960170409e05d8639d
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-10-20 03:24:18 +0530

    added CreateBasketEndpoints

--- Services/Basket/Basket.Api/Features/Basket/CreateBasket/CreateBasketEndpoints.cs
+++ Services/Basket/Basket.Api/Features/Basket/CreateBasket/CreateBasketEndpoints.cs
@@ -1,5 +1,21 @@
 ﻿namespace Basket.Api.Features.Basket.CreateBasket;
+public sealed record CreateBasketRequest(
+    );
+public sealed record CreateBasketResponse(Guid Id);
 
-public sealed class CreateBasketEndpoints
+
+public sealed class CreateBasketEndpoints : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("", async () =>
+        {
+
+        })
+        .WithName("CreateBasket")
+        .Produces<CreateBasketResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Basket")
+        .WithDescription("Create Basket");
+    }
 }
