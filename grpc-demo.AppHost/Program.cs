40535b4d1379ce2ea2a21fbddff32bc9e170579c
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-10-23 03:28:16 +0530

    ShppingCart project added to Aspire host

--- grpc-demo.AppHost/Program.cs
+++ grpc-demo.AppHost/Program.cs
@@ -1,7 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Basket_Api>("basket-api");
 
 builder.AddProject<Projects.Discount_Grpc>("discount-grpc");
+
+builder.AddProject<Projects.ShoppingCart_Api>("shoppingcart-api");
 
 builder.Build().Run();
